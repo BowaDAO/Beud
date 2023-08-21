4b6138b718aab9c9cb1c738d1e148fb5331c9d92
Samuel Ibrahim <samuelibrahim3029@gmail.com>
2023-08-21 14:42:47 +0100

    stlyes fix

--- src/containers/Idea.jsx
+++ src/containers/Idea.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Idea = () => {
-  return <div></div>;
+  return (
+    <section className="lg:px-24 lg:py-16 bg-light">
+      <article>
+        <h1>A Brilliant Problem Solving Tech Idea?</h1>
+        <p>
+          Ideas die. Don’t let yours do. We will stand by you to transform your
+          brilliant idea into a fully functional product that solves problems.
+          We will form a team of passionate tech and non-tech talents to
+          co-found and build your idea into a commercial product under the
+          guidance and mentorship of The Builder. Get those proposals together
+          while you are on the waitlist
+        </p>
+      </article>
+      <div></div>
+    </section>
+  );
 };
 
 export default Idea;
--- src/containers/Mission.jsx
+++ src/containers/Mission.jsx
@@ -33,15 +33,15 @@ const Mission = () => {
   return (
     <section className="px-4 py-12 md:px-[111px] md:py-16 lg:py-16 lg:px-24 flex flex-col items-center lg: gap-12">
       <div className="flex flex-col gap-4 items-center text-center lg:max-w-[825px] md:max-w-[571px] ">
-        <h1 className="text-black font-bold text-2xl md:text-[40px] md:leading-[48px]">
+        <h2 className="text-black font-bold text-2xl md:text-[40px] md:leading-[48px]">
           Building A Thousand Unicorn Out Of Africa
-        </h1>
-        <h2 className="text-grey font-medium text-base md:text-lg">
+        </h2>
+        <p className="text-grey font-medium text-base md:text-lg">
           We believe in African brilliance. We understand that many gifted
           individuals are unable to transform their problem-solving ideas into
           functional products while many African tech experts are unable to make
           the most of their technical knowledge
-        </h2>
+        </p>
       </div>
       <div className="grid lg:grid-cols-4 gap-6 md:grid-cols-2 grid-cols-1">
         {missionData.map((item) => {
