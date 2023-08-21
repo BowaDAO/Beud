9db1dd42dc2840ad4e2aa1cac376c261db285ac9
Samuel Ibrahim <samuelibrahim3029@gmail.com>
2023-08-21 15:20:57 +0100

    idea container

--- src/constants/images.jsx
+++ src/constants/images.jsx
@@ -1,3 +1,5 @@
 import heroimage from "../assets/Images/heroimage.png";
+import idea from "../assets/Images/idea.png";
+import techie from "../assets/Images/techie.png";
 
-export default { heroimage };
+export default { heroimage, idea, techie };
--- src/containers/Idea.jsx
+++ src/containers/Idea.jsx
@@ -1,20 +1,25 @@
 import React from "react";
+import { images } from "../constants";
 
 const Idea = () => {
   return (
-    <section className="lg:px-24 lg:py-16 bg-light">
-      <article>
-        <h1>A Brilliant Problem Solving Tech Idea?</h1>
-        <p>
+    <section className="flex items-center lg:flex-row flex-col lg:px-24 pt-12 md:pt-16 bg-light lg:gap-[128px] md:gap-12 gap-8 md:px-[124px] px-4">
+      <article className="flex flex-col lg:items-start items-center lg:text-start text-center gap-4 md:gap-8 lg:max-w-[638px]">
+        <h1 className="text-2xl md:text-[40px] lg:leading-[48px] text-black font-bold lg:max-w-[398px] md:max-w-[509px]">
+          A Brilliant Problem Solving Tech Idea?
+        </h1>
+        <p className="text-lg font-medium text-grey">
           Ideas die. Don’t let yours do. We will stand by you to transform your
           brilliant idea into a fully functional product that solves problems.
           We will form a team of passionate tech and non-tech talents to
           co-found and build your idea into a commercial product under the
           guidance and mentorship of The Builder. Get those proposals together
-          while you are on the waitlist
+          while you are on the waitlist.
         </p>
       </article>
-      <div></div>
+      <div className="max-w-[482px]">
+        <img src={images.idea} className="w-full" />
+      </div>
     </section>
   );
 };
