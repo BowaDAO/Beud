dd471f29f1622847f166b04b1983ef024016cf85
Samuel Ibrahim <samuelibrahim3029@gmail.com>
2023-08-22 19:04:22 +0100

    nav update

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -3,7 +3,7 @@ import { icons } from "../constants";
 
 const NavigationBar = () => {
   return (
-    <nav className="h-[94px] bg-main lg:px-24 lg:py-4 md:p-6 p-4 flex justify-between items-center">
+    <nav className="h-20 md:h-[94px] bg-main lg:px-24 lg:py-4 md:p-6 p-4 flex justify-between items-center">
       <h1 className="text-[38px] font-bold text-white">Beud</h1>
 
       <span className="hidden lg:inline">
