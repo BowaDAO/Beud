3cde937541248ef53b0aabe59b44446eb5a43d49
Samuel Ibrahim <samuelibrahim3029@gmail.com>
2023-08-24 21:54:59 +0100

    card dynamic styling implementation

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -4,30 +4,34 @@ const MissionCard = ({ thumbNail, title, body }) => {
   let backgroundColor;
   let color;
   if (title === "our vision") {
-    backgroundColor = `#9CBAA6`;
-    color = `#F1FFF6`;
+    backgroundColor = "primary";
+    color = "light";
   }
   if (title === "talent") {
-    backgroundColor = `#F1FFF6`;
-    color = `#536258`;
+    backgroundColor = "light";
+    color = "grey";
   }
   if (title === "network") {
-    backgroundColor = `#9CBAA6`;
-    color = `#F1FFF6`;
+    backgroundColor = "primary";
+    color = "white";
   }
   if (title === "aspiration") {
-    backgroundColor = `#F1FFF6`;
-    color = `#536258`;
+    backgroundColor = "white";
+    color = "grey";
   }
+
+  //f1ff6 light
+  //9cbaa6 primary
+  //536258 grey
 
   return (
     <div
-      className={`max-w-[294px] h-[319px] border-[2px] border-black bg-[${backgroundColor}] flex flex-col items-start gap-8 p-[17.5px]`}
+      className={`max-w-[294px] h-[319px] border-[2px] border-black bg-${backgroundColor} flex flex-col items-start gap-8 p-[17.5px]`}
     >
       <img src={thumbNail} className="h-[50px] w-[55px] " />
       <article className="flex flex-col items-start gap-[22px]">
         <h4 className="text-main text-xl font-semibold capitalize">{title}</h4>
-        <p className={`md:text-lg text-base font-medium text-[${color}]`}>
+        <p className={`md:text-lg text-base font-medium text-${color}`}>
           {body}
         </p>
       </article>
