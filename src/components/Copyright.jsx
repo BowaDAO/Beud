29ae6de7435dba64dc5c79162d826b2981ec39f8
Samuel Ibrahim <samuelibrahim3029@gmail.com>
2023-09-04 19:21:56 +0100

    FAQ card update

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -4,7 +4,7 @@ const Copyright = () => {
   const date = new Date().getFullYear();
 
   return (
-    <p className="text-white text-base md:text-lg font-medium">
+    <p className="text-white text-[12px] md:text-lg font-medium">
       Copyright &copy; {date} Beud<span className="text-secondary">.</span> All
       Rights Reserved
     </p>
--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -11,26 +11,22 @@ const FAQ = ({ answer, question }) => {
   };
 
   return (
-    <article className="md:w-[786px] lg:w-[924px]">
+    <div className="flex flex-col gap-4 md:w-[786px] lg:w-[924px] rounded-[5px] bg-primary px-4 py-6 ">
       <div
         onClick={handleShow}
-        className="flex justify-between items-center lg:gap-3 bg-primary px-4 py-2 rounded-[5px] h-16 md:h-[72px] lg:h-[80px] gap-2"
+        className="flex justify-between items-center lg:gap-3  gap-2"
       >
         <h6 className="text-black text-base md:text-lg">{question}</h6>
         <button>
           <img
             src={show ? icons.minus : icons.plus}
-            className="md:h-6 md:w-6 h-4 w-6"
+            className="md:h-6 md:w-6 h-4 w-6 object-contain"
           />
         </button>
       </div>
 
-      {show && (
-        <article className=" md:py-8 px-4 py-6 bg-main rounded-[5px]">
-          <p className="text-light text-base md:text-lg">{answer}</p>
-        </article>
-      )}
-    </article>
+      {show && <p className="text-black text-base md:text-lg">{answer}</p>}
+    </div>
   );
 };
 
