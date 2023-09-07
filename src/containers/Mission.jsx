66bc4ace4151bcf1fc014257c16aadf11c28da47
Samuel Ibrahim <samuelibrahim3029@gmail.com>
2023-09-07 12:11:31 +0100

    new mission header

--- src/containers/Mission.jsx
+++ src/containers/Mission.jsx
@@ -8,7 +8,7 @@ const Mission = () => {
     <section className="px-4 py-12 md:px-[111px] md:py-16 lg:py-16 lg:px-24 flex flex-col items-center lg: gap-12">
       <div className="flex flex-col gap-4 items-center text-center lg:max-w-[825px] md:max-w-[571px] ">
         <h2 className="text-black font-bold text-2xl md:text-[40px] md:leading-[48px] max-w-[335px] md:max-w-[554px] ">
-          Building A Hundred Unicorn Out Of Africa
+          Building A Hundred Unicorns Out Of Africa
         </h2>
         <p className="text-grey font-medium text-base md:text-lg">
           Beuder believes in African brilliance and understands that many gifted
